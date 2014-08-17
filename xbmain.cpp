c22f1db9e032efaf12345ca6f8551832f28edc76
aatyler <a.a.tyler2@gmail.com>
2014-08-17 09:35:25 -0400

    Starting test 1: Once complete, mv xbmain.cpp -> test1.cpp and maintain for records.

--- xbmain.cpp
+++ xbmain.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include "xbeeDMapi.h"
 #include "TTYserial.h"
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <iterator>
+
+/*
+   Test 1 (17 Auguest, 2014)
+   	The purpose of this test is to do a complete test of all classes/functions
+	written thus far. 
+
+	Functions under test / results:
+
+	xbeeDMapi :
+		rcvPkt
+		(make/load)BCPkt
+		(make/load)UnicastPkt
+		ATNDPkt
+	xbeeNeighbors :
+		update
+		neighborCount
+		operator[]
+		remove
+		clear
+*/
 
 int main()
 {
